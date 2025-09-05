f58380a5fc59e40edab8abe00caaf919f058d9dc
henriklocke <henriklocke@hotmail.com>
2025-09-05 14:59:52 -0700

    Add files via upload
    # Version 2: -Corrected discharge import to use '"average","time-weighted"'
    #            -Added option to make new dfs0 (without extending existing).
    #            -Fill with delete value, not 0 and do not do addition/multiplication on delete value
    #            -Note new rainfall will give an error because rainfall depth + step accumulated fails but that is what it should be.

--- PI_model_automation.py
+++ PI_model_automation.py
@@ -1,7 +1,10 @@
 # TOOL 1 - GeneratePi
 
-# Version 1: Pi functionality added
-
+# Version 1: Pi functionality added.
+# Version 2: -Corrected discharge import to use '"average","time-weighted"'
+#            -Added option to make new dfs0 (without extending existing).
+#            -Fill with delete value, not 0 and do not do addition/multiplication on delete value
+#            -Note new rainfall will give an error because rainfall depth + step accumulated fails but that is what it should be.
 
 
 import openpyxl
@@ -10,24 +13,36 @@ from datetime import datetime
 from datetime import timedelta
 import os
 import pandas as pd
+import numpy as np
 import xlwings as xw
 import mikeio
+from mikecore.DfsFile import DataValueType
+from mikeio import ItemInfo, EUMType, EUMUnit
 from PI_model_automation_variables import * #import everything
 
 df_input_all = pd.read_excel(inputsheet_path)
+
+delete_value = -1e-35
 
 if generate_pi:
     timestep_dict = {}
     timestep_dict["5m"]=300
     
-    pi_end_time = datetime.today().replace(minute=0, second=0, microsecond=0) # today's time round down to nearest hour
-    
+    if custom_end_date == '':
+        pi_end_time = datetime.today().replace(minute=0, second=0, microsecond=0) # today's time round down to nearest hour
+    else:
+        pi_end_time = custom_end_date
     interval = "5m"
     
     pi_dict = {}
     pi_dict['HGL'] = '"average","time-weighted"'
     pi_dict['Rainfall'] = '"total","event-weighted"'
-    pi_dict['Flow'] = '"total","event-weighted"'
+    pi_dict['Flow'] = '"average","time-weighted"'
+    
+    dfs_dict = {}
+    dfs_dict['HGL'] = [EUMType.Water_Level,DataValueType.Instantaneous]
+    dfs_dict['Rainfall'] = [EUMType.Rainfall_Depth,DataValueType.StepAccumulated]#This gives an error but better than wrong value. To be reported.
+    dfs_dict['Flow'] = [EUMType.Discharge,DataValueType.Instantaneous]
     
     for index, row in df_input_all.iterrows():
         
@@ -41,11 +56,15 @@ if generate_pi:
         item_type = row["Type"]
         unit = row["Unit"]
         desc = f"{type} ({unit}) in gauge {name}"
-    
-        dfs0_path = row["DFS0 Path"]
-        dfs = mikeio.read(dfs0_path)
-        dfs0_end_time = dfs.end_time
-        pi_start_time = dfs0_end_time + timedelta(minutes=5)
+
+        if extend_existing:    
+            dfs0_path = row["DFS0 Path"]
+            dfs = mikeio.read(dfs0_path)
+            dfs0_end_time = dfs.end_time
+            pi_start_time = dfs0_end_time + timedelta(minutes=5)
+        else:
+            pi_start_time = datetime.strptime(custom_start_date, '%Y-%m-%d')
+            
         
         ws["A1"] = "Start"
         ws["B1"] = pi_start_time.strftime("%Y-%m-%d %H:%M:%S")
@@ -110,15 +129,19 @@ if generate_pi:
         
 if generate_dfs0:
     for dfs0_path in list(df_input_all["DFS0 Path"].unique()):
-        df_input = df_input_all[df_input_all["DFS0 Path"]==dfs0_path]
         
         #Should be moved out of this loop so multiple PI sheets can go into 1 dfs0
-        dfs = mikeio.read(dfs0_path)
-        dfs_df = dfs.to_dataframe()
+        if extend_existing:
+            df_input = df_input_all[df_input_all["DFS0 Path"]==dfs0_path]
+            dfs = mikeio.read(dfs0_path)
+            dfs_df = dfs.to_dataframe()
+        else:
+            df_input = df_input_all
         
         for index, row in df_input.iterrows():
             if not pd.isna(row.Tag):
                 name = row["Name"]
+                item1_name = row["DFS0 Item 1 Name"]
                 var_type = row["Type"]
                 pi_file_path = output_folder + "\\" + name + "-" + var_type + ".xlsx"
                 print(pi_file_path)
@@ -132,47 +155,44 @@ if generate_dfs0:
                 pi_df.reset_index(drop = True, inplace = True)
                 pi_df.rename(columns = {"Name":"DateTimeInitial"}, inplace = True)
                 pi_df['DateTime'] = pd.to_datetime(pi_df.DateTimeInitial, errors='coerce')
-                pi_df[name] = pd.to_numeric(pi_df[name], errors='coerce').fillna(0)
+                pi_df[name] = pd.to_numeric(pi_df[name], errors='coerce').fillna(delete_value)
                 pi_df.dropna(inplace=True)
                 pi_df.drop(columns=['DateTimeInitial'],inplace=True)
                 
                 if not pd.isna(row['DFS0 Item 1 Addition']):
-                    pi_df[name] = pi_df[name] + row['DFS0 Item 1 Addition']
+                    pi_df[name] = np.where(pi_df[name] != delete_value, pi_df[name] + row['DFS0 Item 1 Addition'], pi_df[name])
                 
                 if not pd.isna(row['DFS0 Item 1 Multiplier']):
-                    pi_df[name] = pi_df[name] * row['DFS0 Item 1 Multiplier']
+                    pi_df[name] = np.where(pi_df[name] != delete_value, pi_df[name] * row['DFS0 Item 1 Multiplier'], pi_df[name])
                     
+                if extend_existing:
+                    pi_df.rename(columns={name:name + '_Transfer'},inplace=True)
+                      
+                    pi_end_time = pi_df.DateTime.max()
+                    rng = pd.date_range(dfs_df.index.min(),pi_end_time,freq='300s')
+        
+                    #join df with dfs_df by DateTime, then transfer the VW14 values from df to dfs_df. 
+            
+                    #Make sure it is an outer join to maintain all dfs0 times in the dfs_df
                 
-                pi_df.rename(columns={name:name + '_Transfer'},inplace=True)
-                
-    
-    
-    
-    
-                pi_end_time = pi_df.DateTime.max()
-                rng = pd.date_range(dfs_df.index.min(),pi_end_time,freq='300s')
-    
-                #join df with dfs_df by DateTime, then transfer the VW14 values from df to dfs_df. 
-    
-    
-    
-                #Make sure it is an outer join to maintain all dfs0 times in the dfs_df
-    
-    
-                ix = pd.DatetimeIndex(rng)
-                dfs_df = dfs_df.reindex(ix)
-                dfs_df['DateTime'] = dfs_df.index
-    
-                dfs_df = pd.merge(dfs_df,pi_df,how='left',on=['DateTime'])
-    
-                dfs_df.set_index('DateTime',inplace=True)
-    
-                dfs_df[name] = dfs_df[name].fillna(dfs_df[name + '_Transfer'])
-    
-                dfs_df.drop(columns=[name + '_Transfer'],inplace=True)
-    
-        dfs_df.to_dfs0(f"{output_folder}\\{name}_Extended.dfs0", 
-                    items=dfs.items, 
-                    title=f"{name} Extended21"
-                )
+                    ix = pd.DatetimeIndex(rng)
+                    dfs_df = dfs_df.reindex(ix)
+                    dfs_df['DateTime'] = dfs_df.index        
+                    dfs_df = pd.merge(dfs_df,pi_df,how='left',on=['DateTime'])        
+                    dfs_df.set_index('DateTime',inplace=True)        
+                    dfs_df[item1_name] = dfs_df[item1_name].fillna(dfs_df[name + '_Transfer'])       
+                    dfs_df.drop(columns=[name + '_Transfer'],inplace=True)                    
+                    dfs_df.to_dfs0(f"{output_folder}\\{name}_Extended.dfs0", 
+                                   items=dfs.items, title=name)
+                else:
+                    eum_type = dfs_dict[var_type][0]
+                    data_value_type = dfs_dict[var_type][1]
+                    # item = ItemInfo(f'{name} - {var_type}',eum_type,data_value_type)
+                    item = ItemInfo(f'{name} - {var_type}',eum_type)
+                    items = [item]
+                    pi_df.set_index('DateTime',inplace=True)
+                    pi_df.to_dfs0(f"{output_folder}\\{name} - {var_type}.dfs0",
+                                  items=[item],title=name)
+                    
+
     
--- PI_model_automation_variables.py
+++ PI_model_automation_variables.py
@@ -1,6 +1,15 @@
+extend_existing = False
 generate_pi = True
 generate_dfs0 = True
 
-inputsheet_path = r"J:\TOOLS\PI_Model_Automation\Pi Inputs_short.xlsx"
+inputsheet_path = r"J:\TOOLS\PI_Model_Automation\Pi Inputs_GE_Tanks.xlsx"
+
+# inputsheet_path = r"J:\TOOLS\PI_Model_Automation\Pi Inputs_short.xlsx"
+
 output_folder = r"J:\TOOLS\PI_Model_Automation\STEP 1"
 
+
+#Dates must be in format like '2025-1-14' with no time. 
+#End date can be empty string in which case present date is used
+custom_start_date = '2024-10-1'
+custom_end_date = ''
